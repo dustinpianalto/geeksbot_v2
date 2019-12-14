a318f129c92cea4c785cc06c46d55cb8f689e4d8
Dustin Pianalto <dustin@djpianalto.com>
2019-12-14 01:07:25 -0900

    Add user_events module and user_update event

---
+++ geeksbot/exts/user_events.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+import logging
+
+
+user_logger = logging.getLogger('UserEvents')
+
+
+class UserEvents(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listner()
+    async def on_user_update(self, before, after):
+        data = {
+            'username': after.name,
+            'discriminator': after.discriminator,
+            'animated': after.is_avatar_animated(),
+            'avatar': after.avatar or str(after.default_avatar)
+        }
+        resp = await self.bot.aio_session.put(f'{self.bot.api_base}/users/{after.id}/',
+                                              headers=self.bot.auth_header,
+                                              json=data)
+        user_logger.info(f'User Update Response:\n{await resp.json()}')
+
+
+def setup(bot):
+    bot.add_cog(UserEvents(bot))
